587624d677d4143f0894004c9844428f62afe811
Kyryl Boroda <kirilboroda25@gmail.com>
2023-06-15 20:52:39 +0300

    asd

---
+++ src/main/java/webs/hillel/Client.java
@@ -0,0 +1,14 @@
+package webs.hillel;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.net.Socket;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.Scanner;
+
+public class Client {
+
+}
